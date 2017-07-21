d716b2ef596e7fd582d1a4c389402f2210276e03
Huser <Huser@MININT-OH7C3JN>
2017-07-21 09:21:24 -0400

    Updare

--- src/test/java/com/fanniemae/testcases/SDETTestCase.java
+++ src/test/java/com/fanniemae/testcases/SDETTestCase.java
@@ -1,10 +1,13 @@
 package com.fanniemae.testcases;
 
+import java.io.IOException;
+
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.fanniemae.base.TestBase;
+import com.fanniemae.utilities.TestUtil;
 
 public class SDETTestCase extends TestBase{
 	
@@ -15,7 +18,7 @@ public class SDETTestCase extends TestBase{
 
 	  }
 	  @Test
-	  public void loginPage() throws InterruptedException {
+	  public void loginPage() throws InterruptedException, IOException {
 		 
 		String loginHomeButton = "id_LOGIN_BUTTON_HOME";
 		String loginButton = "xpath_LOGIN_BUTTON";
@@ -24,9 +27,11 @@ public class SDETTestCase extends TestBase{
 		
 		Assert.assertTrue(isElementPresent(By.id(OR.getProperty("id_LOGIN_BUTTON_HOME"))));
 		click(loginHomeButton);
+		TestUtil.captureScreenshot();
 		type(username,"sdet@sdettraining.com");
 		type(password,"Password");
 		click(loginButton);
+		TestUtil.captureScreenshot();
 		Thread.sleep(3000);
 		
 	  }
---
+++ src/test/java/com/fanniemae/utilities/TestUtil.java
@@ -0,0 +1,30 @@
+package com.fanniemae.utilities;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import com.fanniemae.base.TestBase;
+
+public class TestUtil extends TestBase {
+
+	public static String screenshotPath;
+	public static String screenshotName;
+
+	public static void captureScreenshot() throws IOException {
+
+		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+		Date d = new Date();
+		screenshotName = d.toString().replace(":", "_").replace(" ", "_") + ".jpg";
+
+		FileUtils.copyFile(scrFile,
+				new File(System.getProperty("user.dir") + "\\target\\surefire-reports\\html\\" + screenshotName));
+		
+		System.out.println(screenshotName + " SCREENSHOT TAKEN Successfully!!!");
+	}
+}

8d7905a88e24ac27c932b8f990fd3ec0028b5c4d
Huser <Huser@MININT-OH7C3JN>
2017-07-21 11:02:22 -0400

    Update

--- src/test/java/com/fanniemae/runner/RunCukeTest.java
+++ src/test/java/com/fanniemae/runner/RunCukeTest.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = {"src/test/resources/com\fannie/features"},
+		features = {"src/test/resources/com/fannie/features"},
 		glue = {"/test/java/com/fanniemae/steps"}
 //		plugin = { 
 //					"pretty",

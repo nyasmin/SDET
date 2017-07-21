d06c97a53972fb35195f2a1ecf45afdc9239953a
Huser <Huser@MININT-OH7C3JN>
2017-07-21 10:59:32 -0400

    POM Chnages

--- src/test/java/com/fanniemae/runner/RunCukeTest.java
+++ src/test/java/com/fanniemae/runner/RunCukeTest.java
@@ -7,15 +7,15 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = {"src/com/github/feature"},
-		glue = {"com/github/steps"},
+		features = {"src/test/resources/com\fannie/features"},
+		glue = {"/test/java/com/fanniemae/steps"}
 //		plugin = { 
 //					"pretty",
 //					"html:target/cucumber-reports",
 //					"json:target_json/cucumber.json",
 //	    	        "junit:target_junit/cucumber_junit.xml"
 //				},
-		tags = {"@ddInvalidLogin"}
+		
 		
 		)
 public class RunCukeTest {

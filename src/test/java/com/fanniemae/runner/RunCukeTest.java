afa4ff27160b54861dfbfac32a7e4bbc2e99d71d
Huser <Huser@MININT-OH7C3JN>
2017-07-21 11:09:22 -0400

    ,,

--- src/test/java/com/fanniemae/runner/RunCukeTest.java
+++ src/test/java/com/fanniemae/runner/RunCukeTest.java
@@ -8,7 +8,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = {"src/test/resources/com/fannie/features"},
-		glue = {"src/test/java/com/fanniemae/steps"}
+		glue = "com.fanniemae.steps"
 //		plugin = { 
 //					"pretty",
 //					"html:target/cucumber-reports",
